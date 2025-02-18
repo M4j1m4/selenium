f7d307f36d9adedd0f532fa1c5d1e923930b25b9
M4j1m4 <abesamiskheni@gmail.com>
2025-02-19 04:27:17 +0800

    created new print lines

--- xd.py
+++ xd.py
@@ -13,4 +13,6 @@ elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
+driver.close()
 print("dsaasdasa")
+print("daskhjashkjfsahasfjkl;")
